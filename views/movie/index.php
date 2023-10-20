457cb981340b005d5e8a4556c7d1ab1e0f3627b6
CharlesLLM <charlesllm@orange.fr>
2023-10-20 15:43:39 +0200

    min

--- views/movie/index.php
+++ views/movie/index.php
@@ -17,7 +17,7 @@
                                 <?php foreach ($movies as $movie) { ?>
                                     <li>
                                         <a href="<?= "/test-mvc/movie/view/".$movie->getId() ?>" class="py-3.5 w-full flex items-center text-blue-500 hover:text-blue-700 hover:bg-blue-50">
-                                            <span class="ml-5 mr-2.5 w-1 h-7 bg-blue-500 rounded-r-md hover:text-blue-dark"></span>
+                                            <span class="ml-5 mr-2.5 w-1 h-7 bg-blue-500 rounded-md hover:text-blue-dark"></span>
                                             <?= $movie->getTitle() ?>
                                         </a>
                                     </li>
