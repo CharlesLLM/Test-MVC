b8165bd2885192538c42d809f5277df60db69d24
CharlesLLM <charlesllm@orange.fr>
2023-10-20 14:16:03 +0200

    movie index front

--- views/movie/index.php
+++ views/movie/index.php
@@ -12,17 +12,19 @@
                 <div class="flex-grow">
                     <h3 class="font-bold px-2 py-3 leading-tight">Films</h3>
                     <div class="w-full">
-                        <?php foreach ($movies as $movie) { ?>
-                            <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                                <div class="w-8 h-10 text-center py-1">
-                                    <p class="text-3xl p-0 text-grey-dark">&bull;</p>
-                                </div>
-                                <div class="w-4/5 h-1000 py-3 px-1">
-                                    <a href="<?= "/test-mvc/movie/view/".$movie->getId() ?>" class="hover:text-blue-dark"><?= $movie->getTitle() ?></a>
-                                </div>
-                            </div>
-                        <?php } ?>
-                        <div class="relative">
+                        <div class="mt-2 relative z-10">
+                            <ul class="my-8 mx-auto max-w-xs text-left font-medium text-lg leading-none border-blue-200 divide-y divide-blue-200">
+                                <?php foreach ($movies as $movie) { ?>
+                                    <li>
+                                        <a href="<?= "/test-mvc/movie/view/".$movie->getId() ?>" class="py-3.5 w-full flex items-center text-blue-500 hover:text-blue-700 hover:bg-blue-50">
+                                            <span class="ml-5 mr-2.5 w-1 h-7 bg-blue-500 rounded-r-md hover:text-blue-dark"></span>
+                                            <?= $movie->getTitle() ?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                        <div class="relative px-2 pb-3">
                             <a href="/test-mvc/movie/create" class="mt-2 py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:text-blue-500">&plus; Nouveau film</a>
                         </div>
                     </div>
